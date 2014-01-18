b85b877f2755d2f82f23e5782c1858486b1a743f
0x17 <andreschnabel@me.com>
2014-01-18 18:14:44 +0100

    Moved genetic algorithm for activity list optimization into separate file/module.

---
+++ ActivityListGA.fs
@@ -0,0 +1,37 @@
+﻿namespace RCPSP
+
+open Utils
+open TopologicalSorting
+
+module ActivityListGA =
+    let optimizeActivityList jobs preds utility =
+        let exchange λ rix oix =
+            let r = List.nth λ rix
+            let o = List.nth λ oix
+            List.mapi (fun i e -> if i = rix then o else if i = oix then r else e) λ
+
+        let exchangeFeasible λ rix oix =
+            let len = List.length λ
+            if rix < 0 || oix < 0 || rix >= len || oix >= len then false
+            else feasibleTopSort jobs preds (exchange λ rix oix)
+
+        let mutate =
+            let f n = (pown -1 (n+1)) * (n/2 + 1)
+            fun λ ->
+                let rix = rand 0 (List.length λ)
+                let oscSeq = Seq.initInfinite (fun n -> rix + f n)
+                let oix = Seq.find (fun i -> exchangeFeasible λ rix i || i > List.length λ) oscSeq
+                if oix > List.length λ then λ
+                else exchange λ rix oix
+
+        //let cross λ1 λ2 =
+            //let rval = rand 0 (List.length λ1)
+
+        let mutationStep acc =
+            printf "Step"
+            let mutations = [1..10] |> List.map (fun i -> mutate acc)
+            List.maxBy (fun m -> utility m) (acc :: mutations)
+
+        let nsteps = 100
+        foldItselfTimes mutationStep (topSort jobs preds) nsteps
+
--- ProjectStructure.fs
+++ ProjectStructure.fs
@@ -156,39 +156,6 @@ type ProjectStructure(jobs, durations, demands, preds: int -> Set<int>,
 
         Seq.fold scheduleJob Map.empty λ
 
-    let cleverSsgsHeuristicGAOrdering () =
-        let exchange λ rix oix =
-            let r = List.nth λ rix
-            let o = List.nth λ oix
-            List.mapi (fun i e -> if i = rix then o else if i = oix then r else e) λ
-
-        let exchangeFeasible λ rix oix =
-            let len = List.length λ
-            if rix < 0 || oix < 0 || rix >= len || oix >= len then false
-            else feasibleTopSort jobs preds (exchange λ rix oix)
-
-        let mutate =
-            let f n = (pown -1 (n+1)) * (n/2 + 1)
-            fun λ ->
-                let rix = rand 0 (List.length λ)
-                let oscSeq = Seq.initInfinite (fun n -> rix + f n)
-                let oix = Seq.find (fun i -> exchangeFeasible λ rix i || i > List.length λ) oscSeq
-                if oix > List.length λ then λ
-                else exchange λ rix oix
-
-        //let cross λ1 λ2 =
-            //let rval = rand 0 (List.length λ1)
-
-        let mutationStep acc =
-            printf "Step"
-            let mutations = [1..10] |> List.map (fun i -> mutate acc)
-            List.maxBy (fun m -> profit <| cleverSsgsHeuristic m) (acc :: mutations)
-
-        let nsteps = 100
-        let bestλ = foldItselfTimes mutationStep topOrdering nsteps
-
-        cleverSsgsHeuristic bestλ
-
     member ps.Profit = profit
 
     member ps.BackwardsGapFillHeuristic λ () =
@@ -249,10 +216,12 @@ type ProjectStructure(jobs, durations, demands, preds: int -> Set<int>,
 
     member ps.CleverSSGSHeuristicDefault () = cleverSsgsHeuristic topOrdering
     member ps.CleverSSGSHeuristic = cleverSsgsHeuristic
-    member ps.CleverSsgsHeuristicGAOrdering = cleverSsgsHeuristicGAOrdering
+    member ps.CleverSsgsHeuristicGAOrdering =
+        let bestλ = ActivityListGA.optimizeActivityList jobs preds (profit << cleverSsgsHeuristic)
+        cleverSsgsHeuristic bestλ
 
     member ps.CleverSSGSHeuristicAllOrderings () =
-        let winner = Seq.maxBy (ps.Profit << cleverSsgsHeuristic) (allTopSorts jobs preds)
+        let winner = Seq.maxBy (profit << cleverSsgsHeuristic) (allTopSorts jobs preds)
         cleverSsgsHeuristic winner    
 
     member ps.CleverSSGSHeuristicOrderingStats optimalSts =
