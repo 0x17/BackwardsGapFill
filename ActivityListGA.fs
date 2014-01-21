c86c1e7b014b8215fccf6548abbd01ca35ee9fbc
André Schnabel <andreschnabel@me.com>
2014-01-21 12:28:35 +0100

    Fixed time leak.

--- ActivityListGA.fs
+++ ActivityListGA.fs
@@ -22,17 +22,31 @@ module ActivityListGA =
             if oix = len then λ
             else exchange λ rix oix
 
-        // Profile
         let mutationStep population =
+            printf "Step"
             let mutations = population |> List.map (fun individual -> foldItselfTimes mutate individual (rand 1 10))
             let allConsidered = (population @ mutations)
             let curMax = List.maxBy utility allConsidered
             let maxUtility = utility curMax
-            List.filter (fun m -> utility m = maxUtility) allConsidered
+            let bestIndividuals = List.filter (fun m -> utility m = maxUtility) allConsidered
+            (maxUtility, bestIndividuals)       
 
-        let initialPopulation = (topSort ps.Jobs ps.Preds) :: (PriorityRules.allRules |> List.map (fun pr -> pr ps))
+        let rec folder f pair =
+            let npair = f (snd pair)
+            if fst npair = fst pair then snd npair
+            else folder f npair
 
-        foldItselfConverge mutationStep initialPopulation
+        let maxUtil individuals = List.map utility individuals |> List.max
+
+        let initialPopulation =
+            let feasibles =
+                (topSort ps.Jobs ps.Preds) :: (PriorityRules.allRules |> List.map (fun pr -> pr ps))
+                |> List.filter (feasibleTopSort ps.Jobs ps.Preds)
+            let maxUtility = maxUtil feasibles
+            feasibles
+            |> List.filter (fun i -> utility i = maxUtility)
+
+        folder mutationStep (maxUtil initialPopulation, initialPopulation)
 
     let optimizeHeuristic (ps:ProjectStructure) =
         let utility = (ps.Profit << ps.CleverSSGSHeuristic)
